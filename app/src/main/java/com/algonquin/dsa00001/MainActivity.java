f1717b1e76ef8a2a59666c16c0733833dbaacd7c
CLifford <dsa00001@algonquinlive.com>
2021-10-07 02:14:22 -0400

    Finished week 4

--- app/src/main/java/com/algonquin/dsa00001/MainActivity.java
+++ app/src/main/java/com/algonquin/dsa00001/MainActivity.java
@@ -2,39 +2,79 @@ package com.algonquin.dsa00001;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.animation.Animation;
-import android.view.animation.LinearInterpolator;
-import android.view.animation.RotateAnimation;
-import android.widget.ImageView;
-import android.widget.Switch;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imgView;
-    Switch sw;
+    private static String TAG = "MainActivity";
+    private String emailAddressToString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.w(TAG, "onCreate() - Loading Widgets");
 
-        imgView = findViewById(R.id.flagview);
-        sw = findViewById(R.id.spin_switch);
+        final EditText emailAddress = (EditText) findViewById(R.id.email_address);
+        final SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        String value = prefs.getString("loginName", "null");
+        Toast.makeText(MainActivity.this, "result 0 is :" + value, Toast.LENGTH_LONG).show();
 
-        sw.setOnCheckedChangeListener((btn, isChecked) -> {
-            if (isChecked)
-            {
-                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-                rotate.setDuration(5000);
-                rotate.setRepeatCount(Animation.INFINITE);
-                rotate.setInterpolator(new LinearInterpolator());
 
-                imgView.startAnimation(rotate);
+            emailAddress.setText(value);
+            emailAddressToString = emailAddress.getText().toString();
+            Toast.makeText(MainActivity.this, "result 2 is :" + emailAddressToString, Toast.LENGTH_LONG).show();
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = prefs.edit();
+                //editor.putString("loginName", "cliffiswatching@tv.ca");
+                editor.putString("loginName", emailAddressToString);
+                editor.apply();
+
+                Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+                nextPage.putExtra("EmailAddress", emailAddressToString);
+                startActivity(nextPage);
             }
-            else {
-                imgView.clearAnimation();
-            }
-
         });
-    }}
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w(TAG, "onStart() -The application is now visible on screen");
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w(TAG, "onResume() - The application is now responding to user input");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(TAG, "onPause() - The application is now partially visible to user ");
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w(TAG, "onDestroy() - The application is now dismissed and in background ");
+    }
+
+
+}
